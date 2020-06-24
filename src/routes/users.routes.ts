963dbd83f89630f0a1165dcbf5d8118925d68ec1
Denis Souza <denyssouza1@hotmail.com>
2020-06-23 22:59:34 -0300

    first commit

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
+import CreateUserService from '../services/CreateUserService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const userRouter = Router();
+const upload = multer(uploadConfig);
+
+userRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+userRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
+
+export default userRouter;
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository } from 'typeorm';
+import { startOfHour } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import Appointment from '../models/Appointment';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment alredy is booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
